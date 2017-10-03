d8a472e1a9b7e4d63bd2054a133d64ef401e5991
Dheeraj <dheeraj.pal@rubiconred.com>
2017-10-03 23:44:54 +0530

    initial commit

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -21,6 +21,7 @@ export const routes: Routes = [
       },
       { path: 'home', loadChildren: 'app/+home/home.module#HomeModule', data: { pageTitle: 'Home' } },
       { path: 'dashboard', loadChildren: 'app/+dashboard/dashboard.module#DashboardModule', data: { pageTitle: 'Dashboard' } },
+      { path: 'product', loadChildren: 'app/+product/product.module#ProductModule', data: { pageTitle: 'Product' } },
       // {path: 'smartadmin', loadChildren: 'app/+smartadmin-intel/smartadmin-intel.module#SmartadminIntelModule',data:{pageTitle: 'Smartadmin'}},
       // {path: 'app-views', loadChildren: 'app/+app-views/app-views.module#AppViewsModule',data:{pageTitle: 'App Views'}},
       // {path: 'calendar', loadChildren: 'app/+calendar/calendar.module#CalendarModule',data:{pageTitle: 'Calendar'}},
